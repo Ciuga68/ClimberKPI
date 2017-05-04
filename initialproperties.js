28c7ad85330a8d852cddf7efe3d6b66655daf96c
CLIMBER\clkaf <karl.sjostrand@climber.se>
2017-05-04 12:22:11 +0200

    Version 1.0.0

---
+++ initialproperties.js
@@ -0,0 +1,1 @@
+define([],function(){"use strict";return{showTitles:!1,showDetails:!1,createdWithVersion:"1.0.0",propertiesVersion:"1.0.0",qHyperCubeDef:{qMeasures:[],qInitialDataFetch:[{qWidth:3,qHeight:3e3}]}}});
