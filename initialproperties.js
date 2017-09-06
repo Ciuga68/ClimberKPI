7640e26f67304e65e5d71dbf6dc8b5fc2bd20027
Micke <mikael.gerhardsson@gmail.com>
2017-09-06 10:24:19 +0200

    1.2.1

--- initialproperties.js
+++ initialproperties.js
@@ -1,1 +1,1 @@
-define([],function(){"use strict";return{showTitles:!1,showDetails:!1,createdWithVersion:"1.2.0",propertiesVersion:"1.2.0",qHyperCubeDef:{qMeasures:[],qInitialDataFetch:[{qWidth:3,qHeight:3e3}]}}});
+define([],function(){"use strict";return{showTitles:!1,showDetails:!1,createdWithVersion:"1.2.1",propertiesVersion:"1.2.1",qHyperCubeDef:{qMeasures:[],qInitialDataFetch:[{qWidth:3,qHeight:3e3}]}}});
