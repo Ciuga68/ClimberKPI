f7be157781d0d32e030bc925bfea4181f3dda85e
Edward Hallgren <edward.hallgren@gmail.com>
2018-03-11 13:31:27 +0100

    1.4.1

--- initialproperties.js
+++ initialproperties.js
@@ -1,1 +1,1 @@
-define([],function(){"use strict";return{showTitles:!1,showDetails:!1,createdWithVersion:"1.4.0",propertiesVersion:"1.4.0",qHyperCubeDef:{qMeasures:[],qInitialDataFetch:[{qWidth:3,qHeight:3e3}]}}});
+define([],function(){"use strict";return{showTitles:!1,showDetails:!1,createdWithVersion:"1.4.1",propertiesVersion:"1.4.1",qHyperCubeDef:{qMeasures:[],qInitialDataFetch:[{qWidth:3,qHeight:3e3}]}}});
