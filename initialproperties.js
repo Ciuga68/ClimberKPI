64a81d8c5409ec5298dce1e323f69ac061245e34
CLIMBER\clalt <axel.lojdquist@climber.se>
2018-06-28 09:48:48 +0200

    v1.5.0

--- initialproperties.js
+++
@@ -1,1 +0,0 @@
-define([],function(){"use strict";return{showTitles:!1,showDetails:!1,createdWithVersion:"1.4.5",propertiesVersion:"1.4.5",qHyperCubeDef:{qMeasures:[],qInitialDataFetch:[{qWidth:3,qHeight:3e3}]}}});
